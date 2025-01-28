4c30bdb0e404da3a9ffb5fece791a830719c200a
Connor Schweitzer <ConnorSchweitzer@gmail.com>
2025-01-27 22:11:51 -0500

    Final submission for JS challenges in foundations section

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+    if(yr%4==0&&(yr%100!=0||(yr%100==0&&yr%400==0))){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celsius = (5/9)*(fahr-32)
+  return Math.round(celsius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsi) {
+  let fahr = (9/5)*(celsi)+32
+  return Math.round(fahr*10)/10;
 };
 
 // Do not edit below this line
